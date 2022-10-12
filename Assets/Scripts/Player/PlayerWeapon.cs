7bc4b4f7abbc2c84836ccb586fc59a093e2923e9
AIE\s211406 <Cody.pase@gmail.com>
2022-10-12 14:28:03 +1100

    Player Weapon script 80% done

--- Assets/Scripts/Player/PlayerWeapon.cs
+++ Assets/Scripts/Player/PlayerWeapon.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerWeapon : MonoBehaviour
 {
 
-    private Animation animation;
-    public float cooldown;
+    private Animator animation;
+    public float cooldown = 2f;
+    float lastClickedTime = 0;
+    int numberOfClicks = 0;
 
     void Start()
     {
@@ -14,11 +16,28 @@ public class PlayerWeapon : MonoBehaviour
     }
     void Update()
     {
-        
+        if (animation.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && animation.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {
+            animation.SetBool("Attack", false);
+            numberOfClicks = 0;
+        }
+
+        if (Time.time - lastClickedTime > cooldown)
+        {
+
+        }
     }
 
     void OnClick()
     {
+        lastClickedTime = Time.time;
+        numberOfClicks++;
+        if (numberOfClicks == 1)
+        {
+            animation.SetBool("Attack", true);
+        }
+        numberOfClicks = Mathf.Clamp(numberOfClicks, 0, 3);
 
+        //if (numberOfClicks >= 2 && animation.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && animation.GetCurrentAnimatorStateInfo(0).IsName("Attack")) {animation.SetBool("attack", false);}
     }
 }
