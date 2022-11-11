e8e0e7b0cc01add783467692059656e6f9fbf1a9
harrybosch03@gmail.com <harrybosch03@gmail.com>
2022-11-11 13:55:44 +1100

    Added Conveyor Caching

--- Assets/Scripts/Level/ConveyorBuilder.cs
+++ Assets/Scripts/Level/ConveyorBuilder.cs
@@ -18,6 +18,8 @@ public class ConveyorBuilder : MonoBehaviour
     [SerializeField] int repeatX;
     [SerializeField] bool reverse;
 
+    public List<ConveyorBuilder> builders;
+
     public float Length { get => length; set => length = value; }
 
     private void OnValidate()
@@ -34,6 +36,25 @@ public class ConveyorBuilder : MonoBehaviour
         if (Application.isPlaying) return;
 
         MeshFilter filter = GetComponent<MeshFilter>();
+
+        if (!builders.Contains(this))
+        {
+            builders.Add(this);
+        }
+
+        foreach (var builder in builders)
+        {
+            if (builder == this) continue;
+
+            if (Mathf.Abs(builder.length - length) < 0.1f)
+            {
+                if (builder.repeatX == repeatX)
+                {
+                    filter.sharedMesh = builder.GetComponent<MeshFilter>().sharedMesh;
+                    return;
+                }
+            }
+        }
 
         Quaternion r = Quaternion.Euler(rotation);
         float smin = float.MaxValue;
